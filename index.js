7ae58c672908ab0acf7785def9650fdc9f2f47c3
Pablo Sanchez <pablo.sanchez@aranova.es>
2018-12-12 11:54:53 +0100

    fixed path

--- index.js
+++ index.js
@@ -1,75 +1,31 @@
 // Dependencies
 var url  = require('url');
 var path = require('path');
+var send = require('send');
 var mime = require('mime');
-var fs = require('fs');
-var zlib = require('zlib');
-var cache = {}
-module.exports = function(options){
-	return function($){
-		var pathname = $.url.pathname;
-		var mimeType = mime.lookup(pathname);
-		var extension = path.extname(pathname);
-		var dateOffset = 604800000;
-		
-		// if no route was specified there is an extension and mimeType is not binary
-		if(extension){
-			// set header
-			$.header('Content-Type', mimeType);
-			$.status(200);
-			var source = options.path + $.url.pathname;
-			fs.stat(source, function (error, stats) {
-				if(!error){
-					$.header('Last-Modified', new Date(stats.mtime).toUTCString())
-					$.header('Expires', new Date(new Date().getTime() + dateOffset).toUTCString())
-					$.header('Cache-Control', 'public')
-					var modified_since = new Date($.headers['if-modified-since']).getTime();
-					var last_modified = new Date(stats.mtime).getTime()
-					if(!$.headers['if-modified-since'] || last_modified > modified_since){
-						
-						// file was modified
-						fs.readFile(source, function(readerror, data){
-							if(readerror) throw readerror
-							
-							if(mimeType == 'text/css' || mimeType == 'application/javascript'){	
-								var buffer = new Buffer(data);
-								zlib.gzip(buffer, function(error, gzip){
-									if(error) throw error
-									cache[source] = gzip;
-									$.header('Content-Encoding', 'gzip')
-									$.header('Vary', 'Accept-Encoding')
-									$.passed = false;
-									$.responded = true;
-									$.response.end(gzip)
-									$.return()
-								})
-							} else {
-								$.passed = false;
-								$.responded = true;
-								$.response.end(data)
-								$.return()
-							}
-						})
-					
-					} else {
-						// not modified
-						$.status(304)
-						$.responded = true;
-						$.response.end()
-						$.return()
-					}
-					
-				} else if (error.type != 'ENOENT') {
-					$.status(error.status || 500, 'File not found');
-					$.return();
-				} else {
-					throw error;
-					$.return()
-				}
-				
-			});
-		} else {
-			$.return();
-		}
-	}
+var options = module.parent.options;
+
+exports.global = function($){
+  var root = options.root || '/static';
+  var pathname = $.url.pathname;
+  var mimeType = mime.lookup(pathname);
+  var extension = path.extname(pathname);
+  // if no route was specified there is an extension and mimeType is not binary
+  if($.noRoute && extension && mimeType != 'application/octet-stream'){
+    // set header
+    $.header('content-type', mimeType);
+    $.status(200);
+    
+    // send static file
+    send($.request, $.url.pathname, { root: root })
+      .on('error', function(err) {
+        //console.log(err);
+        $.status(err.status || 500, 'File not found.');
+        $.return();
+      }).pipe($.response); 
+  } else {
+    $.return();
+  }
 }
+
+module.parent.return();
